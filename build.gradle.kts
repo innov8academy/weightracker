9b4f20b5ee497759bc2414dd84bd6f1586096258
alextomandrews <alextomandrews@gmail.com>
2025-04-18 12:18:32 +0000

    Move pluginManagement block to settings.gradle.kts. Fixed the root cause of all the previous errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,4 @@
 
-pluginManagement {
-    repositories {
-        google()
-        gradlePluginPortal()
-        mavenCentral()
-    }
-}
-
 plugins {
     id("org.jetbrains.kotlin.android") version "1.9.21"
     id("com.android.application") version "8.2.0"
