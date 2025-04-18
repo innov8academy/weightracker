959746360be30e1815b9a909a58294e0069f8622
alextomandrews <alextomandrews@gmail.com>
2025-04-18 09:56:59 +0000

    Fix build.gradle.kts and add plugin management block

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
 
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
 plugins {
-    kotlin("android") version "1.9.21"
+    kotlin("android") version "1.9.21" apply false
+    id("com.android.application") version "8.2.0" apply false
 }
