9f7ba41383edcc427bd0b408706a1476b4fd6fb6
alextomandrews <alextomandrews@gmail.com>
2025-04-18 11:11:38 +0000

    Fix duplicated plugins block and remove apply false from root build

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,6 @@ pluginManagement {
 }
 
 plugins {
-    kotlin("android") version "1.9.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21"
     id("com.android.application") version "8.2.0" apply false
 }
